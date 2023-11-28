fd973f02654c867350a4b860ab421da8aff24c7f
Raphael N. Bressam <71392326+bateaquidev@users.noreply.github.com>
2023-11-28 10:01:35 -0300

    feat: String Extensions

---
+++ src/CrossCutting/Extensions/StringExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+
+namespace CrossCutting.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string ToBase64String(this string input)
+        {
+            if (string.IsNullOrEmpty(input))
+                return input;
+
+            var inputBytes = Encoding.UTF8.GetBytes(input);
+            var inputBase64String = Convert.ToBase64String(inputBytes);
+            return inputBase64String;
+        }
+
+        public static string FromBase64String(this string input)
+        {
+            if (string.IsNullOrEmpty(input))
+                return input;
+
+            var inputBytes = Convert.FromBase64String(input);
+            var result = Encoding.UTF8.GetString(inputBytes);
+            return result;
+        }
+    }
+}
